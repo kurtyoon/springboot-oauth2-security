a563b970b250b40eff5742b95fa1f74063433b48
kurtyoon <akakertcobain@naver.com>
2023-12-01 16:28:38 +0900

    Revert "[Feat] timezone 설정"
    This reverts commit 462f9911b4cccf555dd39dc2ba3c9df1d273e69d.

--- src/main/java/dev/spring/security/oauth2/Oauth2Application.java
+++ src/main/java/dev/spring/security/oauth2/Oauth2Application.java
@@ -1,18 +1,10 @@
 package dev.spring.security.oauth2;
 
-import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.TimeZone;
-
 @SpringBootApplication
 public class Oauth2Application {
-
-	@PostConstruct
-	public void init() {
-		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Oauth2Application.class, args);
