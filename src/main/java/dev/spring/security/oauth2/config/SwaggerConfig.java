f16ead62cfa82877ceb77f19c59ea5ff24bc6f1d
kurtyoon <akakertcobain@naver.com>
2023-12-05 20:20:19 +0900

    [Feat] Swagger Configuration 작성

---
+++ src/main/java/dev/spring/security/oauth2/config/SwaggerConfig.java
@@ -0,0 +1,28 @@
+package dev.spring.security.oauth2.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public-api")
+                .pathsToMatch("/**")
+                .build();
+    }
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("OAuth2 API")
+                        .description("OAuth2 API Docs")
+                        .version("0.0.1")
+                );
+    }
+}
