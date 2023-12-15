9433caca6a97462b6f3190616652355657e5360c
kurtyoon <akakertcobain@naver.com>
2023-12-16 00:17:34 +0900

    [Feat] 회원가입과 로그인 정보를 JSON으로 가져온다.

--- src/main/java/dev/spring/security/oauth2/dto/auth/AuthLoginDto.java
+++ src/main/java/dev/spring/security/oauth2/dto/auth/AuthLoginDto.java
@@ -3,17 +3,19 @@ package dev.spring.security.oauth2.dto.auth;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 
 public record AuthLoginDto(
         @JsonProperty("social_id")
-        @Size(min = 4, max = 20, message = "socialId는 4자 이상 20자 이하로 입력해주세요.")
-        @NotNull(message = "socialId는 필수 입력값입니다.")
+        @Pattern(
+                regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*_).{4,20}$",
+                message = "아이디는 4자 이상 20자 이하로 입력해주세요."
+        )
+        @NotNull(message = "아이디는 필수 입력값입니다.")
         String socialId,
 
         @JsonProperty("password")
         @Pattern(
-                regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%]).{10,20}$",
+                regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%]).{8,16}$",
                 message = "비밀번호는 대문자 1개 이상, 소문자 1개 이상, 숫자 1개 이상, 특수문자(!, @, #, %, $) 1개 이상으로 구성된 10~20자리 비밀번호로 입력해주세요."
         )
         @NotNull(message = "password는 필수 입력값입니다.")
